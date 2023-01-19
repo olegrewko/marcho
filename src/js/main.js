3ab363b44f867ddef15d188f222a41ee78e846b6
olegrewko <olegrewko@gmail.com>
2023-01-19 23:50:30 +0300

    r

--- src/js/main.js
+++ src/js/main.js
@@ -46,7 +46,22 @@ $(function () {
 
   });
 
+
   $('.celect-style').styler();
+
+  $('.shop-content__filter-btn').on('click', function(){
+
+    $('.shop-content__filter-btn').removeClass('.shop-content__filter-btn--active');
+    $(this).addClass('shop-content__filter-btn--active');
+  });
+
+  $('.button-list').on('click', function(){
+    $('.product-item').addClass('product-item--list');
+  });
+
+  $('.button-grid').on('click', function(){
+    $('.product-item').removeClass('product-item--list');
+  });
 
   function getTimeRemaining(endtime) {
     const total = Date.parse(endtime) - Date.parse(new Date());
